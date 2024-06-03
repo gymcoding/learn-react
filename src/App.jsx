5b1b82b3554e2dabb4b3d97023f08ebf6a1a7787
gymcoding <bruce.lean17@gmail.com>
2024-06-03 15:56:00 +0900

    06 실습 강의 컴포넌트

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
 import './App.css'
-import Header from './components/Header'
-import Main from './components/Main'
-import Footer from './components/Footer'
 
 function App() {
   return (
     <>
-      <Header />
-      <Main />
-      <Footer />
+      <img src='/img/htmlcss.png' />
     </>
   )
 }
