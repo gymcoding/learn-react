7480b3af4a11c67f8d2b6df45b638616269f8014
gymcoding <bruce.lean17@gmail.com>
2024-06-09 11:49:18 +0900

    09 JSX에서 자바스크립트 사용

--- src/components/course/CourseItem.jsx
+++ src/components/course/CourseItem.jsx
@@ -1,10 +1,26 @@
 export default function CourseItem(props) {
+
+  const course = {
+    title: '입문자를 위한, HTML&CSS 웹 개발 입문',
+    description: '웹 개발에 필요한 기본 지식을 배웁니다.',
+    image: './img/htmlcss.png',
+    alt: '강의 이미지'
+  };
+  
+  const isEmpty = false;
+
+  if (isEmpty) {
+    return (
+      <p>강의가 없습니다.</p>
+    )
+  }
+
   return (
     <article className="course">
-      <img className="course__img" src="./img/htmlcss.png" alt="" />
+      <img className="course__img" src={course.image} alt={course.alt} />
       <div className="course__body">
-        <div className="course__title">입문자를 위한, HTML&CSS 웹 개발 입문</div>
-        <div className="course__description">웹 개발에 필요한 기본 지식을 배웁니다. </div>
+        <div className="course__title">{course.title}</div>
+        <div className="course__description">{course.description}</div>
       </div>
     </article>
   );
--- src/components/course/CourseListCard.jsx
+++ src/components/course/CourseListCard.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 import CourseItem from './CourseItem'
 
 function CourseListCard(props) {
+  
   return (
-    <div className="card">
+    <div className="card" style={{ backgroundColor: 'black', color: 'white' }}>
       <div className="card__header">강의 목록</div>
       <div className="card__body">
         <div className="courses">
