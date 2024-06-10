560b444ef72132388d8f03c3177026a1a21a0d18
gymcoding <bruce.lean17@gmail.com>
2024-06-10 16:40:55 +0900

    11 컴포넌트에 props 전달하기

--- src/components/course/CourseItem.jsx
+++ src/components/course/CourseItem.jsx
@@ -1,26 +1,11 @@
-export default function CourseItem(props) {
-
-  const course = {
-    title: '입문자를 위한, HTML&CSS 웹 개발 입문',
-    description: '웹 개발에 필요한 기본 지식을 배웁니다.',
-    image: './img/htmlcss.png',
-    alt: '강의 이미지'
-  };
-  
-  const isEmpty = false;
-
-  if (isEmpty) {
-    return (
-      <p>강의가 없습니다.</p>
-    )
-  }
+export default function CourseItem({ title, description, thumbnail }) {
 
   return (
     <article className="course">
-      <img className="course__img" src={course.image} alt={course.alt} />
+      <img className="course__img" src={thumbnail} alt="강의 이미지" />
       <div className="course__body">
-        <div className="course__title">{course.title}</div>
-        <div className="course__description">{course.description}</div>
+        <div className="course__title">{title}</div>
+        <div className="course__description">{description}</div>
       </div>
     </article>
   );
--- src/components/course/CourseListCard.jsx
+++ src/components/course/CourseListCard.jsx
@@ -1,15 +1,43 @@
 import CourseItem from './CourseItem'
 
 function CourseListCard(props) {
-  
+  const course1 = {
+    title: '입문자를 위한, HTML&CSS 웹 개발 입문',
+    description: '웹 개발에 필요한 기본 지식을 배웁니다.',
+    thumbnail: '/img/htmlcss.png',
+  };
+  const course2 = {
+    title: '입문자를 위한, ES6+ 최신 자바스크립트 입문',
+    description: '쉽고! 알찬! 내용을 준비했습니다.',
+    thumbnail: '/img/js.png',
+  };
+  const course3 = {
+    title: '포트폴리오 사이트 만들고 배포까지!',
+    description: '포트폴리오 사이트를 만들고 배포해 보세요.',
+    thumbnail: '/img/portfolio.png',
+  };
+
   return (
-    <div className="card" style={{ backgroundColor: 'black', color: 'white' }}>
+    // style={{ backgroundColor: 'black', color: 'white' }}
+    <div className="card">
       <div className="card__header">강의 목록</div>
       <div className="card__body">
         <div className="courses">
-          <CourseItem />
-          <CourseItem />
-          <CourseItem />
+          <CourseItem
+            title={course1.title}
+            description={course1.description}
+            thumbnail={course1.thumbnail}
+          />
+          <CourseItem
+            title={course2.title}
+            description={course2.description}
+            thumbnail={course2.thumbnail}
+          />
+          <CourseItem
+            title={course3.title}
+            description={course3.description}
+            thumbnail={course3.thumbnail}
+          />
         </div>
       </div>
     </div>
