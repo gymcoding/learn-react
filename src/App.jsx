6f95cad8384bee4cdc15bed3f87c6b5edf3ad3c1
gymcoding <bruce.lean17@gmail.com>
2024-06-11 15:34:03 +0900

    12.1 조건부 렌더링

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,19 @@ function App() {
       title: '입문자를 위한, HTML&CSS 웹 개발 입문',
       description: '웹 개발에 필요한 기본 지식을 배웁니다.',
       thumbnail: '/img/htmlcss.png',
+      isFavorite: true
     },
     {
       title: '입문자를 위한, ES6+ 최신 자바스크립트 입문',
       description: '쉽고! 알찬! 내용을 준비했습니다.',
       thumbnail: '/img/js.png',
+      isFavorite: false
     },
     {
       title: '포트폴리오 사이트 만들고 배포까지!',
       description: '포트폴리오 사이트를 만들고 배포해 보세요.',
       thumbnail: '/img/portfolio.png',
+      isFavorite: true
     }
   ]
   return (
--- src/components/course/CourseItem.jsx
+++ src/components/course/CourseItem.jsx
@@ -1,11 +1,28 @@
-export default function CourseItem({ title, description, thumbnail }) {
+function HeartIconBtn({ isFavorite = false }) {
+  if (isFavorite) {
+    return (
+      <button className="btn">
+        <img className="icon-heart" src="/img/heart-fill-icon.svg" />
+      </button>
+    )
+  }
+  return (
+    <button className="btn">
+      <img className="icon-heart" src="/img/heart-icon.svg" />
+    </button>
+  )
+}
 
+export default function CourseItem({ title, description, thumbnail, isFavorite }) {
   return (
     <article className="course">
       <img className="course__img" src={thumbnail} alt="강의 이미지" />
       <div className="course__body">
         <div className="course__title">{title}</div>
         <div className="course__description">{description}</div>
+      </div>
+      <div className="course__icons">
+        <HeartIconBtn isFavorite={isFavorite} />
       </div>
     </article>
   );
