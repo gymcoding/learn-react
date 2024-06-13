c977f6b523ef1bea85049fa84012bce582e88aa4
gymcoding <bruce.lean17@gmail.com>
2024-06-13 17:42:32 +0900

    13.1 강의 목록 리스트 렌더링

--- src/components/course/CourseListCard.jsx
+++ src/components/course/CourseListCard.jsx
@@ -3,20 +3,10 @@ import CourseItem from './CourseItem'
 
 function CourseListCard({ items }) {
 
-  const [course1, course2, course3] = items;
-
   return (
     <Card title='강의 목록'>
       <div className='courses'>
-        <CourseItem
-          {...course1}
-        />
-        <CourseItem
-          {...course2}
-        />
-        <CourseItem
-          {...course3}
-        />
+        {items.map(item => <CourseItem {...item} />)}
       </div>
     </Card>
   );
