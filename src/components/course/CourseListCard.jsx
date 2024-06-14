02783219cbaa8f9ffcc1bf93f65b90cd8dc8bd61
gymcoding <bruce.lean17@gmail.com>
2024-06-14 12:19:49 +0900

    13.4 <Fragment> 사용하여 렌더링

--- src/components/course/CourseListCard.jsx
+++ src/components/course/CourseListCard.jsx
@@ -1,12 +1,20 @@
+import { Fragment } from 'react';
 import Card from '../Card';
 import CourseItem from './CourseItem'
 
 function CourseListCard({ title, items }) {
 
+  const lastIndex = items.length - 1;
+
   return (
     <Card title={title}>
       <div className='courses'>
-        {items.map(item => <CourseItem key={item.id} {...item} />)}
+        {items.map((item, index) => (
+          <Fragment key={item.id}>
+            <CourseItem {...item} />
+            {index !== lastIndex && <hr className="divider" />}
+          </Fragment>
+        ))}
       </div>
     </Card>
   );
