780643985c301251d9df3a006ee7bf41d6ecb6b4
gymcoding <bruce.lean17@gmail.com>
2024-06-25 21:09:50 +0900

    17.6 TodoList 수정

--- src/components/todo/TodoList.jsx
+++ src/components/todo/TodoList.jsx
@@ -1,17 +1,12 @@
-import React from 'react';
-
-function TodoList({ todos = [] }) {
+function TodoList({ todos = []}) {
   const items = [...todos];
-  items.push({ id: 2, label: '포트폴리오 사이트 만들기' });
+  items.push({ id: 2, label: '포트폴리오 사이트 만들기'});
+
   return (
     <ul>
-      {items.map(story => (
-        <li key={story.id}>
-          {story.label}
-        </li>
-      ))}
+      {items.map(item =>  <li key={item.id}>{item.label}</li>)}
     </ul>
-  );
+  )
 }
 
 export default TodoList;
