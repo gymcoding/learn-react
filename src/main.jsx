28dd4e0f0ffa9602c82fab36c31c47fc7ee6c076
gymcoding <bruce.lean17@gmail.com>
2024-06-27 10:44:34 +0900

    20.1 마우스 포인터 움직이기 - 동작 안함

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './AppCounter.jsx'
+import App from './AppMovingDot.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
