cacf73a4b5c174c9e59770745635c3635693afd9
gymcoding <bruce.lean17@gmail.com>
2024-07-15 13:32:37 +0900

    31.6 실습 - TodoProvider 컴포넌트

--- src/AppTodo.jsx
+++ src/AppTodo.jsx
@@ -3,25 +3,16 @@ import './App.css'
 import TodoList from './components/todo/TodoList';
 import AddTodo from './components/todo/AddTodo';
 
-import todoReducer from './reducer/todo-reducer';
-import { useImmerReducer } from 'use-immer';
-import { TodoContext, TodoDispatchContext } from './context/TodoContext';
+import { TodoProvider } from './context/TodoContext';
 
 function AppTodo() {
   
-  const [todos, dispatch] = useImmerReducer(todoReducer, [
-    {id: 0, text: "HTML&CSS 공부하기", done: false },
-    {id: 1, text: "자바스크립트 공부하기", done: false },
-  ]);
-  
   return (
-    <TodoContext.Provider value={todos}>
-      <TodoDispatchContext.Provider value={dispatch}>
-        <h2>할일목록</h2>
-        <AddTodo />
-        <TodoList />
-      </TodoDispatchContext.Provider>
-    </TodoContext.Provider>
+    <TodoProvider>
+      <h2>할일목록</h2>
+      <AddTodo />
+      <TodoList />
+    </TodoProvider>
   );
 }
 
--- src/context/TodoContext.jsx
+++ src/context/TodoContext.jsx
@@ -1,4 +1,21 @@
 import { createContext } from "react";
+import { useImmerReducer } from "use-immer";
+import todoReducer from "../reducer/todo-reducer";
 
 export const TodoContext = createContext(null);
 export const TodoDispatchContext = createContext(null);
+
+export function TodoProvider({ children }) {
+  const [todos, dispatch] = useImmerReducer(todoReducer, [
+    {id: 0, text: "HTML&CSS 공부하기", done: false },
+    {id: 1, text: "자바스크립트 공부하기", done: false },
+  ]);
+  
+  return (
+    <TodoContext.Provider value={todos}>
+      <TodoDispatchContext.Provider value={dispatch}>
+        {children}
+      </TodoDispatchContext.Provider>
+    </TodoContext.Provider>
+  );
+}
