a35c5217b3037971e834683c9429d2aa4084c455
gymcoding <bruce.lean17@gmail.com>
2024-07-17 13:50:42 +0900

    33.2 실습 - 데이터 조회

--- src/AppEffect.jsx
+++ src/AppEffect.jsx
@@ -1,15 +1,32 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function AppEffect(props) {
 
+  // 1] DOM 조작하기
+  // useEffect(() => {
+  //   const $h2 = document.querySelector('#title');
+  //   $h2.textContent = 'Data Fetching';
+  // }, []);
+
+  const [list, setList] = useState([]);
+
   useEffect(() => {
-    const $h2 = document.querySelector('#title');
-    $h2.textContent = 'Data Fetching';
+    fetch('data/courses_all.json')
+    .then((res) => res.json())
+    .then(data => {
+      console.log('✅ 데이터 조회 성공 ')
+      setList(data);
+    })
   }, []);
 
   return (
     <>
-      <h2 id="title">데이터 가져오기</h2> 
+      <h2 id="title">데이터 가져오기</h2>
+      <ul>
+        {list.map((item) => (
+          <li key={item.id}>{item.title}</li>
+        ))}
+      </ul>
     </>
   );
 };
