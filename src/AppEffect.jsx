1bffd856bec592d060b36479fb2a0d65a2924587
gymcoding <bruce.lean17@gmail.com>
2024-07-17 14:00:19 +0900

    33.3 실습 - 의존성 배열

--- src/AppEffect.jsx
+++ src/AppEffect.jsx
@@ -9,19 +9,36 @@ export default function AppEffect(props) {
   // }, []);
 
   const [list, setList] = useState([]);
+  const [filter, setFilter] = useState('all');
 
   useEffect(() => {
-    fetch('data/courses_all.json')
+    fetch(`data/courses_${filter}.json`)
     .then((res) => res.json())
     .then(data => {
       console.log('✅ 데이터 조회 성공 ')
       setList(data);
     })
-  }, []);
+  }, [filter]);
 
   return (
     <>
       <h2 id="title">데이터 가져오기</h2>
+      <label htmlFor="all">전체</label>
+      <input
+        id="all"
+        type="radio"
+        value="all"
+        checked={filter === 'all'}
+        onChange={(e) => setFilter(e.target.value)}
+      />
+      <label htmlFor="favorite">좋아요</label>
+      <input
+        id="favorite"
+        type="radio"
+        value="favorite"
+        checked={filter === 'favorite'}
+        onChange={(e) => setFilter(e.target.value)}
+      />
       <ul>
         {list.map((item) => (
           <li key={item.id}>{item.title}</li>
